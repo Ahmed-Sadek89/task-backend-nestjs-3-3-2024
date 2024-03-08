b618a01ea36d0a6dd56d30cfbdceaa88936d2469
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2024-03-08 19:53:33 +0200

    enable cors with config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.FRONTEND_DEPLOYED_LINK, process.env.FRONTEND_DEVELOPMENT_LINK], // Allow these origins
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+    exposedHeaders: ['Authorization'], // Expose these headers
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  });
   await app.listen(4000);
 }
 bootstrap();
